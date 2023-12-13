0f318126f4d4af619ce06a9e3beca8b12b47af81
durgeshbijewar <137867028+durgeshbijewar@users.noreply.github.com>
2023-12-13 02:11:57 -0500

    Add files via upload

---
+++ web3.js
@@ -0,0 +1,23 @@
+//demontrate how we can intrect with smart contract using web3.js
+
+//node -v
+//npm -v
+//npm init -y
+//now  we open with the code
+//package.json
+//now wo go to gitbase here
+//npm intall --save web3
+//now chek dependentis in editer
+//node
+//let Web3=require("web3");
+//web3
+//is the complete web3 dependentis
+//let crete varible
+//let web3=new web3(new.providers.HttpProviders("ganche http"));
+//web3.eth.getBalance("acount number")
+//create myfirstsmartcontract
+//create a smartcontract
+
+//let mycontract= new web3.eth.contract([ABI COPY,copy contract])
+//mycontract.methed.get.Distributing().call().then(console.log).
+// this is the smartcontract using web3.js
